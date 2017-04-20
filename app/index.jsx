d1dbd21279fa0ee641d3e284c80608f4ab47f78e
benweizhu <benweizhu@gmail.com>
2017-04-20 23:06:38 +0800

    add svg

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import family from './icons/family.svg';
+import like from './icons/like.svg';
 import styles from './index.scss';
 import classNames from 'classnames';
 
@@ -8,10 +9,11 @@ const cx = classNames.bind(styles);
 
 class SvgComponent extends React.Component {
   render() {
+    console.log(family);
     return (
       <div>
         <div className={cx('icon')} dangerouslySetInnerHTML={{ __html: family }}></div>
-        <div className={cx('icon')} dangerouslySetInnerHTML={{ __html: family }}></div>
+        <div className={cx('icon red')} dangerouslySetInnerHTML={{ __html: like }}></div>
       </div>
     );
   }
